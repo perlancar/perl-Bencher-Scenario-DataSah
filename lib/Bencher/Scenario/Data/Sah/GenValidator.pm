45c93b50391127237e75946083c3904d4b00d1c0
perlancar (@pc-office) <perlancar@gmail.com>
2015-12-23 18:04:21 +0700

    Add scenarios

---
+++ lib/Bencher/Scenario/Data/Sah/GenValidator.pm
@@ -0,0 +1,45 @@
+package Bencher::Scenario::Data::Sah::GenValidator;
+
+# DATE
+# VERSION
+
+my $return_types = ['bool', 'str', 'full'];
+
+our $scenario = {
+    summary => 'Benchmark validator generation',
+    participants => [
+        {
+            name => 'gen_validator',
+            fcall_template => 'Data::Sah::gen_validator(<schema>, {return_type=> <return_type>})',
+        },
+    ],
+    datasets => [
+        {
+            name    => 'type',
+            summary => '',
+            args    => {
+                schema => 'str',
+                'return_type@' => $return_types,
+            },
+        },
+        {
+            name    => 'type+req',
+            summary => '',
+            args    => {
+                schema => 'str*',
+                'return_type@' => $return_types,
+            },
+        },
+        {
+            name    => 'type+2clause',
+            summary => '',
+            args    => {
+                schema => ['str*', min_len=>1, max_len=>10],
+                'return_type@' => $return_types,
+            },
+        },
+    ],
+};
+
+1;
+# ABSTRACT:
---
+++ lib/Bencher/Scenario/Data/Sah/Validate.pm
@@ -0,0 +1,37 @@
+package Bencher::Scenario::Data::Sah::Validate;
+
+# DATE
+# VERSION
+
+my $return_types = ['bool', 'str', 'full'];
+
+our $scenario = {
+    summary => 'Benchmark validation',
+    participants => [
+        {
+            name => 'gen_validator',
+            code_template => 'state $v = Data::Sah::gen_validator(<schema>, {return_type => <return_type>}); $v->(<data>)',
+        },
+    ],
+    datasets => [
+        {
+            name => 'int',
+            args => {
+                schema => 'int',
+                'data@' => [1, "a"],
+                'return_type@' => $return_types,
+            },
+        },
+        {
+            name => 'str+2clause',
+            args => {
+                schema => ['str', min_len=>1, max_len=>5],
+                'data@' => ["abc", ""],
+                'return_type@' => $return_types,
+            },
+        },
+    ],
+};
+
+1;
+# ABSTRACT:
